ff8af454dbd72c45b798e4661aaf23774111c5bd
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-02-08 01:32:46 -0600

    Worked on the category routes.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,46 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include:[{model: Product}]
+  })
+  .then((results)=>{res.json(results)})
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id, {
+    include:[{model: Product}]
+  })
+  .then((results)=>{res.json(results)})
 });
 
 router.post('/', (req, res) => {
   // create a new category
+console.log(req.body)
+  Category.create({
+    category_name: req.body.category_name
+  }).then((results)=>{res.status(200).json(results)}).catch((err)=>{res.status(500).json(err)})
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    category_name: req.body.category_name
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(res.status(200).json());
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  // instance.destroy().then(() => {});
+  Category.destroy({where:{id:req.params.id}}).then(res.status(200).json())
 });
 
 module.exports = router;
